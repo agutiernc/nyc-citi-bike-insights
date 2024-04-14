d2c7190506395a9e8b6e66082ea2e65ac5c886a4
Alfonso Gutierrez <agutiernc@gmail.com>
2024-04-13 18:32:49 -0700

    added progress bar to dlt load pipeline and optimized script. added readme

---
+++ README.md
@@ -0,0 +1,78 @@
+# Biking in the Big Apple: Trends and Insights in New York City's Citi Bike Program
+
+## Project Description
+
+This project analyzes data from New York City's Citi Bike share program for the years 2019, 2020, and 2023. The primary goal is to investigate ridership trends before the 2020 pandemic, during 2020, and in the year 2023. The project aims to answer the following questions:
+
+1. How did the 2020 pandemic impact ridership, and did it rebound significantly by 2023?
+2. Are there any differences or similarities in monthly usage trends for each year, particularly during the 2020 pandemic?
+3. What is the comparison between members of the bike program and non-members in terms of total usage across all three years combined?
+4. Which bike stations are the most popular, and what are the total trips and average trip times for each of the top 5 stations?
+
+By analyzing the data, we can gain insights into the popularity of certain areas in New York City among Citi Bike users and determine if the most popular stations change over time, even as Citi Bike relocates bike stations. The project also examines whether the average trip time for users changes significantly when starting and completing trips at these popular stations.
+
+Through this analysis, we aim to better understand the impact of the pandemic on bike-sharing in New York City and identify any lasting changes in ridership patterns and user behavior.
+
+
+## Tech Stack
+
+- **Terraform**: Used for Infrastructure as Code (IaC) to create a Google Cloud Storage (GCS) bucket and BigQuery dataset
+
+- **Google Cloud Storage (GCS)**: Used as a storage solution for the project data in parquet format
+
+- **Google BigQuery**: Used as the data warehouse for storing and analyzing the Citi Bike data
+
+- **PyArrow**: Used in conjunction with the Data Load Tool (dlt) for the initial data pipeline
+
+- **Data Load Tool (dlt)**: Utilized for creating data pipelines, dbt transformations, and setting up GitHub Actions
+
+- **dbt (Data Build Tool)**: Employed for performing data transformations and running tests on the data stored in BigQuery
+
+- **GitHub Actions**: Integrated with dlt to deploy a monthly triggered data pipeline
+
+- **Google Looker Studio**: Used for creating a dashboard to visualize the analyzed Citi Bike data
+
+<ADD TECH FLOW HERE>
+
+<ADD DASHBOARD HERE>
+
+<ADD TABLE SCHEMAS HERE>
+
+## Table Schemas
+
+### Original Schemas
+| 2019 | 2020 | 2023 |
+| :-- | :--- | :--- |
+| tripduration | tripduration | - |
+| startime | startime | started_at|
+| stoptime | stoptime | ended_at |
+| start station id | start station id | start_station_id |
+| start station name | start station name | start_station_name |
+| start station latitude | start station latitude | start_lat |
+| start station longitude | start station longitude | start_lng |
+| end station id | end station id | end_station_id |
+| end station name | end station name | end_station_name|
+| end station latitude | end station latitude | end_lat |
+| end station longitude | end station longitude | end_lng |
+| bikeid | bikeid | ride_id |
+| usertype | usertype | member_casual |
+| birth year | birth year | - |
+| gender | gender | - |
+| - | - | rideable_type |
+
+### Final Schema
+
+| 2019 - 2023 |
+| :-- |
+| ride_id |
+| start_time_date |
+| stop_time_date |
+| trip_duration_seconds |
+| start_station_name |
+| stop_station_name |
+| user_category |
+
+
+<ADD CHALLENGES HERE>
+
+<ADD INSTRUCTIONS HERE>
--- dlt_data_load/load_bike_data_to_gcs.py
+++ dlt_data_load/load_bike_data_to_gcs.py
@@ -5,69 +5,84 @@ import io, sys, os, time
 import dlt
 import pyarrow as pa
 import pyarrow.csv as pa_csv
+from tqdm import tqdm
 
 os.environ["SCHEMA__NAMING"] = "direct"
 
 YEARS = [19, 20, 23]
 
+@dlt.resource(name="citi_bike_data")
 def download_citi_data(year):
     url = f"https://s3.amazonaws.com/tripdata/20{year}-citibike-tripdata.zip"
-    tables = []
     
     try:
         response = requests.get(url, stream=True)
         response.raise_for_status()
 
-        zip_data = io.BytesIO(response.content)
+        # for progress bar
+        total_size = int(response.headers.get('content-length', 0))
+        block_size = 1024 # 1KB chunk size
 
-        print("downloaded and extracted zip file")
+        with tqdm(total=total_size, unit='iB', unit_scale=True, desc=f"Downloading 20{year} data", colour="blue") as pbar:
+            zip_data = io.BytesIO()
 
-        # Read CSV files in monthly folders
-        with zipfile.ZipFile(zip_data, 'r') as zip_ref:
-            for file_name in zip_ref.namelist():
-                if file_name.endswith('.csv') and not file_name.startswith('__MACOSX'):
-                    print(f"Reading {file_name}")
+            # reading data in chunks
+            for data in response.iter_content(block_size):
+                zip_data.write(data)
+                pbar.update(len(data))
 
-                    with zip_ref.open(file_name) as f:
-                        # Specifying data types for mixed columns
-                        convert_options = pa.csv.ConvertOptions(column_types={
-                            'start_station_id': pa.string(),
-                            'end_station_id': pa.string()
-                        })
+            tqdm.write("Downloaded and extracted zip file")
 
-                        table = pa_csv.read_csv(f, convert_options=convert_options)
-                        tables.append(table)
+            # Read CSV files in monthly folders
+            with zipfile.ZipFile(zip_data, 'r') as zip_ref:
+                total_files = len(zip_ref.namelist())
+
+                with tqdm(total=total_files, desc=f"Processing 20{year} CSV files", colour="green") as file_pbar:
+                    for file_name in zip_ref.namelist():
+                        if file_name.endswith('.csv') and not file_name.startswith('__MACOSX'):
+
+                            with zip_ref.open(file_name) as f:
+                                table = None
+                                convert_options = None
+
+                                if year == 23:
+                                    convert_options = pa_csv.ConvertOptions(column_types={
+                                        "start_station_id": pa.string()
+                                    })
+                                else:
+                                    convert_options = pa_csv.ConvertOptions(column_types={
+                                        "start station id": pa.string(),
+                                        "end station id": pa.string()
+                                    })
+
+                                yield pa_csv.read_csv(f, convert_options=convert_options)
+
+                        file_pbar.update(1)
     except HTTPError as http_err:
-        print(f"HTTP error occurred: {http_err}")
+        tqdm.write(f"HTTP error occurred: {http_err}")
         sys.exit(1)
     except Exception as err:
-        print(f"An error occurred: {err}")
+        tqdm.write(f"An error occurred: {err}")
         sys.exit(1)
-
-    return tables
 
 
 for year in YEARS:
-    pipeline = dlt.pipeline(
-        pipeline_name=f"citi-20{year}",
-        destination="filesystem",
-        dataset_name=f"citi_data_20{year}"
-    )
+    tqdm.write(f"Once processing is done, converting 20{year} data to parquet and uploading to GCS bucket. Please wait...")
 
     tables = download_citi_data(year)
-    combined_table = pa.concat_tables(tables)
-
-    print("Combined tables done. Attempting to convert to parquet and upload to GCS bucket")
-
+        
+    pipeline = dlt.pipeline(
+        pipeline_name=f"citi_20{year}",
+        destination="filesystem",
+        dataset_name="bike_tripdata", # bucket folder
+        progress=dlt.progress.tqdm(colour="yellow")
+    )
+        
     pipeline.run(
-        combined_table,
-        table_name=f"citi_bike_20{year}-tester",
+        tables,
+        table_name=f"bike_20{year}",
         loader_file_format="parquet",
-        write_disposition='replace',
-        progress="tqdm"
+        write_disposition="replace"
     )
 
-    print("Success! Uploaded parquet to GCS!")
-    print("Pausing for 10 seconds")
-
-    time.sleep(10)
+    tqdm.write(f"Success! Uploaded 20{year} parquet to GCS!")
